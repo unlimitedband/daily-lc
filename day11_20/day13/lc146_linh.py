ddcfc8b6806af72bed0f0799165803903f58de16
Linh Pham Duc <linhpduc@gmail.com>
2024-04-23 07:10:48 +0700

    linh: solved lc146 (python)

---
+++ day11_20/day13/lc146_linh.py
@@ -0,0 +1,20 @@
+from collections import OrderedDict
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.pop(key)
+        elif len(self.cache) == self.capacity:
+            self.cache.popitem(last=False)
+        self.cache[key] = value
