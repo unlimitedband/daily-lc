0418c41f86a8b66176c27a24c2383d83669bb245
Trung Hải <cbg.hai.09@gmail.com>
2024-04-24 23:28:07 +0700

    hai: solve lc72 (go)

---
+++ day11_20/day19/lc72_hai.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func minDistance(word1 string, word2 string) int {
+	m, n := len(word1), len(word2)
+	dp := make([][]int, m+1)
+	for i := 0; i <= m; i++ {
+		dp[i] = make([]int, n+1)
+	}
+
+	for i := 0; i <= m; i++ {
+		dp[i][0] = i
+	}
+	for i := 0; i <= n; i++ {
+		dp[0][i] = i
+	}
+
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+			}
+		}
+	}
+
+	return dp[m][n]
+}
+
+func main() {
+	word1 := "intention"
+	word2 := "execution"
+	fmt.Println(minDistance(word1, word2))
+}
